cc61679b5d1921215634c5c0f128a9a41cef6628
Pranava <1rn22cs118.pranavakp@rnsit.ac.in>
2025-02-26 23:34:10 +0530

    Feb25

---
+++ FreshStart/Feb25.java
@@ -0,0 +1,11 @@
+class Feb25 {
+    public static void main(String args[])
+    {
+        System.out.println(args.length);
+        for(int i=0;i<args.length;i++)
+        {
+            System.out.println(args[i]);
+            System.out.println(args[i].length());
+        }
+    }
+}
